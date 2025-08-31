16b8760107b115a23aec1edd4ef2a0bf777fca26
NeoLez <leogonzachi@gmail.com>
2025-08-31 04:43:28 +0000

    Added player

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import pygame
 from constants import *
+from player import *
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
 
 def main():
     print("Starting Asteroids!")
@@ -9,15 +31,36 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+        keys = pygame.key.get_pressed()
+        
+        if(keys[pygame.K_w]):
+            player.move(dt)
+        if(keys[pygame.K_s]):
+            player.move(-dt)
+        
+        updateable.update(dt)
+
         screen.fill((1,1,1))
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 
